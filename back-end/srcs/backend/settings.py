a667a201511ae3e2181aff92a81284aa43d50b39
Georg Home <gkubina@student.lehavre.fr>
2025-01-28 00:12:03 +0100

    save Tue 28 Jan 2025 12:12:03 AM CET

--- back-end/srcs/backend/settings.py
+++ back-end/srcs/backend/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         "NAME": "pongdb",
         "USER": "postgres",
         "PASSWORD": "password",
-        "HOST": "127.0.0.1",        
+        "HOST": "postgres",        
         "PORT": "5432",
     }
 }
