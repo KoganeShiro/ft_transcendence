35eaad14a32a651ecb08a57b0fa273b727bd41d0
Georg Nikolaus Kubina <gkubina@c1r6p9.42lehavre.fr>
2025-02-09 18:11:51 +0100

    save Sun Feb  9 06:11:51 PM CET 2025

--- back-end/srcs/gamestats/serializers.py
+++ back-end/srcs/gamestats/serializers.py
@@ -7,20 +7,20 @@
 #         fields = '__all__'
 
 from rest_framework import serializers
-from .models import Game
+from .models import PongSolo
 from django.contrib.auth.models import User
 
-class GameSerializer(serializers.ModelSerializer):
+class GameSerializer(serializers.ModelSerializer):    
     player1 = serializers.CharField(source='player1.username', read_only=True)
     player2 = serializers.CharField(source='player2.username', read_only=True)
     winner = serializers.CharField(source='winner.username', read_only=True)
     loser = serializers.CharField(source='loser.username', read_only=True)
 
     class Meta:
-        model = Game
+        model = PongSolo
         fields = ['id', 'player1', 'player2', 'winner', 'loser', 'timestamp']
 
 class GameWriteSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Game
+        model = PongSolo
         fields = ['player1', 'player2', 'winner', 'loser']
