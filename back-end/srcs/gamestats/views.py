ba61b45b0bdf354f101a63ca6b594a06cb5dcdb4
Georg Nikolaus Kubina <gkubina@c1r6p9.42lehavre.fr>
2025-02-09 18:41:06 +0100

    save Sun Feb  9 06:41:06 PM CET 2025

--- back-end/srcs/gamestats/views.py
+++ back-end/srcs/gamestats/views.py
@@ -13,6 +13,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import PongSolo
 from .serializers import GameSerializer, GameWriteSerializer
+from rest_framework.response import Response
 
 class GameViewSet(viewsets.ModelViewSet):
     queryset = PongSolo.objects.all().order_by('-timestamp')  
@@ -23,5 +24,32 @@ class GameViewSet(viewsets.ModelViewSet):
             return GameWriteSerializer
         return GameSerializer  # Use read serializer for output
     
+
+class LastFiveGamesView(viewsets.ViewSet):
+    def list(self, request):
+        games = PongSolo.objects.filter(player1=request.user) | PongSolo.objects.filter(player2=request.user)
+        last_five_games = games.order_by('-timestamp')[:5]
+        serializer = GameSerializer(last_five_games, many=True)
+        data = serializer.data
+        for game in data:
+            if game['player1'] == request.user.username:
+                game['opponent'] = game['player2']
+            if game['player2'] == request.user.username:
+                game['opponent'] = game['player1']
+            if game['winner'] == request.user.username:
+                game['won'] = True
+            if game['loser'] == request.user.username:
+                game['won'] = False
+            game['my_score'] = game['player1_score'] if game['player1'] == request.user.username else game['player2_score']
+            game['opponent_score'] = game['player1_score'] if game['player2'] == request.user.username else game['player2_score']
+            game['my_rank'] = game['rank_player1_begin'] if game['player1'] == request.user.username else game['rank_player2_begin']
+            game['opponent_rank'] = game['rank_player1_begin'] if game['player2'] == request.user.username else game['rank_player2_begin']
+            
+        
+            
+
+
+        return Response(data)
+
 
 
