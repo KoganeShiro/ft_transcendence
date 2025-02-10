2fa7d1bd699040b4fea7e6fe917ba63c5eb5de6f
Lapinew <damien.lelievre.work@gmail.com>
2025-02-10 16:04:43 +0100

    Add: add the front + back of the pong game (damien)

---
+++ game-logic/back-gamelogic/chat_project/asgi.py
@@ -0,0 +1,28 @@
+"""
+ASGI config for chat_project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+# from django.urls import re_path
+# from chat.consumers import ChatConsumer
+# from chat.routing import websocket_urlpatterns
+import chat.routing
+import pong.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns + pong.routing.websocket_urlpatterns        )
+    ),
+})
