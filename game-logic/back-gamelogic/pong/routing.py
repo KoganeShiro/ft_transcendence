5ee312b6bf3acd24a4f1070cfeac950ff7076d9f
Lapinew <damien.lelievre.work@gmail.com>
2025-02-12 17:49:56 +0100

    pb docker Damien

--- game-logic/back-gamelogic/pong/routing.py
+++ game-logic/back-gamelogic/pong/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
 from . import consumers
+from . import friends_consumer
 
 websocket_urlpatterns = [
     re_path(r'ws/pong/$', consumers.PongConsumer.as_asgi()),
-    # re_path(r'ws/pong_distance/$', consumers.PongDistanceConsumer.as_asgi()),  # Pour le jeu local
+    re_path(r'ws/friendPong/(?P<room_name>\w+)/$', friends_consumer.FriendPongConsumer.as_asgi()), 
 ]
