454d33908d03c217eb88d336cbe9419115bbd485
Georg Nikolaus Kubina <gkubina@c1r6p9.42lehavre.fr>
2025-02-15 16:54:15 +0100

    save Sat Feb 15 04:54:15 PM CET 2025

--- back-end/srcs/friends/views.py
+++ back-end/srcs/friends/views.py
@@ -161,33 +161,3 @@ def get_last_15_messages(request):
     serializer = messagesSerializer(last_15_messages, many=True)
     
     return Response(serializer.data)
-
-# class FriendshipViewSet(viewsets.ModelViewSet):
-#     queryset = Friendship.objects.all()
-#     serializer_class = FriendshipSerializer
-#     permission_classes = [IsAuthenticated]
-
-# class muted_UserViewSet(viewsets.ModelViewSet):
-#     queryset = muted_User.objects.all()
-#     serializer_class = muted_UserSerializer
-#     permission_classes = [IsAuthenticated]
-
-# class blocked_UserViewSet(viewsets.ModelViewSet):
-#     queryset = blocked_User.objects.all()
-#     serializer_class = blocked_UserSerializer
-#     permission_classes = [IsAuthenticated]
-
-# class messagesViewSet(viewsets.ModelViewSet):
-#     queryset = messages.objects.all()
-#     serializer_class = messagesSerializer
-#     permission_classes = [IsAuthenticated]
-#     def get_serializer_class(self):
-#         if self.request.method in ['POST', 'PUT', 'PATCH']:  # Use write serializer for input
-#             return messagesSerializerWrite
-#         return messagesSerializer  # Use read serializer for output
-    
-#     def get_queryset(self):
-#         user = self.request.user
-#         friends = Friendship.objects.filter(user=user, friend__blocked_user__isnull=True, friend__muted_user__isnull=True)
-#         friend_ids = friends.values_list('friend_id', flat=True)
-#         return messages.objects.filter(sender__in=friend_ids, receiver=user) | messages.objects.filter(sender=user, receiver__in=friend_ids)
